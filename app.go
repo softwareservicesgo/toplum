e546cdf97393d2253fe33e3006e4a81b84487453
NurjemalTekayeva <tekayevanurjemal2005@gmail.com>
2026-05-27 20:04:24 +0500

    update config file for railway

--- app.go
+++ app.go
@@ -19,7 +19,7 @@ import (
 
 func main() {
 	var cfgPath string
-	flag.StringVar(&cfgPath, "c", "./etc/railway_internal_config.yaml", "path to config file") //   "./etc/railway_internal_config.yaml"
+	flag.StringVar(&cfgPath, "c", "./etc/config.yaml", "path to config file") //   "./etc/railway_internal_config.yaml"
 	flag.Parse()
 
 	cfg := config.GetConfig(cfgPath)
